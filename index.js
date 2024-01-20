b7ce52d3db090127ddf8ad43c548378a2aee11ab
code-with-shahzad <malikshahzad454545@gmail.com>
2024-01-20 11:03:38 +0500

    fix number issue

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const handleSubmit = () => {
   isShowError = true;
 };
 const handleChange = (e) => {
-  
   const input_1 = document.getElementById("input_1");
   const input_2 = document.getElementById("input_2");
   const input_3 = document.getElementById("input_3");
@@ -25,11 +24,11 @@ const handleChange = (e) => {
   const tooltip_1 = document.getElementById("tooltip_1");
   const tooltip_2 = document.getElementById("tooltip_2");
   const tooltip_3 = document.getElementById("tooltip_3");
- if (isShowError) {
-   tooltip_1.style.visibility =
-     input_1.value.trim() === "" ? "visible" : "hidden";
-   tooltip_2.style.visibility =
-     input_2.value.trim() === "" ? "visible" : "hidden";
-   tooltip_3.style.visibility = input_3.value === "" ? "visible" : "hidden";
- }
+  if (isShowError) {
+    tooltip_1.style.visibility =
+      input_1.value.trim() === "" ? "visible" : "hidden";
+    tooltip_2.style.visibility =
+      input_2.value.trim() === "" ? "visible" : "hidden";
+    tooltip_3.style.visibility = input_3.value === "" ? "visible" : "hidden";
+  }
 };
